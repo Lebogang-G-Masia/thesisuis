cd87e6130020aed4a89a6d21b31f0a88e8b37a63
Lebogang Masia <lebogangmasia0@gmail.com>
2026-02-13 22:11:42 +0200

    Updated the split function to save memory

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <string_view>
 #include <vector>
 
 #ifndef THESISUIS_UTILS
@@ -8,6 +8,6 @@ void crash();
 // get user input
 std::string userInput(bool = false);
 // Split string using delimeter
-std::vector<std::string> split(std::string&, const std::string& =":");
+std::vector<std::string_view> split(std::string_view&, const std::string_view& =":");
 
 #endif // THESISUIS_UTILS
--- src/utils.cpp
+++ src/utils.cpp
@@ -25,14 +25,12 @@ std::string userInput(bool hide) {
     return input;
 }
 
-std::vector<std::string> split(std::string& s, const std::string& delimiter) {
-    std::vector<std::string> tokens;
+std::vector<std::string_view> split(std::string_view& s, const std::string& delimiter) {
+    std::vector<std::string_view> tokens;
     size_t pos = 0;
-    std::string token;
-    while ((pos = s.find(delimiter)) != std::string::npos) {
-        token = s.substr(0, pos);
-        tokens.push_back(token);
-        s.erase(0, pos + delimiter.length());
+    while ((pos = s.find(delimiter)) != std::string_view::npos) {
+        tokens.push_back(s.substr(0, pos));
+        s.remove_prefix(pos + delimiter.length());
     }
     tokens.push_back(s);
 
